13b2e6335907e18e6eda4f6a60c6f9000f756c93
Alvin Johnson So <so.alvinjohnson@gmail.com>
2021-12-13 20:43:40 +0800

    added PHPCS and resolved issues found with PHPCS

--- tawkto/assets/js/tawk.admin.js
+++ tawkto/assets/js/tawk.admin.js
@@ -1,61 +1,69 @@
 'use strict';
 
-jQuery( function() {
-	document.getElementById( 'defaultOpen' ).click();
+jQuery(
+	function() {
+		document.getElementById( 'defaultOpen' ).click();
 
-	if ( jQuery( '#always-display' ).prop( 'checked' ) ) {
-		jQuery( '.tawk-selected-display' ).hide();
-		jQuery( '#show-onfrontpage' ).prop( 'disabled', true );
-		jQuery( '#show-oncategory' ).prop( 'disabled', true );
-		jQuery( '#show-ontagpage' ).prop( 'disabled', true );
-		jQuery( '#show-onarticlepages' ).prop( 'disabled', true );
-		jQuery( '#include-url' ).prop( 'disabled', true );
-	} else {
-		jQuery( '.tawk-selected-display' ).show();
-	}
-
-	jQuery( '#always-display' ).change( function() {
-		if ( this.checked ) {
-			jQuery( '.tawk-selected-display' ).fadeOut();
+		if ( jQuery( '#always-display' ).prop( 'checked' ) ) {
+			jQuery( '.tawk-selected-display' ).hide();
 			jQuery( '#show-onfrontpage' ).prop( 'disabled', true );
 			jQuery( '#show-oncategory' ).prop( 'disabled', true );
 			jQuery( '#show-ontagpage' ).prop( 'disabled', true );
 			jQuery( '#show-onarticlepages' ).prop( 'disabled', true );
 			jQuery( '#include-url' ).prop( 'disabled', true );
 		} else {
-			jQuery( '.tawk-selected-display' ).fadeIn();
-			jQuery( '#show-onfrontpage' ).prop( 'disabled', false );
-			jQuery( '#show-oncategory' ).prop( 'disabled', false );
-			jQuery( '#show-ontagpage' ).prop( 'disabled', false );
-			jQuery( '#show-onarticlepages' ).prop( 'disabled', false );
-			jQuery( '#include-url' ).prop( 'disabled', false );
+			jQuery( '.tawk-selected-display' ).show();
 		}
-	});
 
-	jQuery( '#exclude-url' ).change( function() {
-		if ( this.checked ) {
+		jQuery( '#always-display' ).change(
+			function() {
+				if ( this.checked ) {
+					jQuery( '.tawk-selected-display' ).fadeOut();
+					jQuery( '#show-onfrontpage' ).prop( 'disabled', true );
+					jQuery( '#show-oncategory' ).prop( 'disabled', true );
+					jQuery( '#show-ontagpage' ).prop( 'disabled', true );
+					jQuery( '#show-onarticlepages' ).prop( 'disabled', true );
+					jQuery( '#include-url' ).prop( 'disabled', true );
+				} else {
+					jQuery( '.tawk-selected-display' ).fadeIn();
+					jQuery( '#show-onfrontpage' ).prop( 'disabled', false );
+					jQuery( '#show-oncategory' ).prop( 'disabled', false );
+					jQuery( '#show-ontagpage' ).prop( 'disabled', false );
+					jQuery( '#show-onarticlepages' ).prop( 'disabled', false );
+					jQuery( '#include-url' ).prop( 'disabled', false );
+				}
+			}
+		);
+
+		jQuery( '#exclude-url' ).change(
+			function() {
+				if ( this.checked ) {
+					jQuery( '#exlucded-urls-container' ).fadeIn();
+				} else {
+					jQuery( '#exlucded-urls-container' ).fadeOut();
+				}
+			}
+		);
+
+		if ( jQuery( '#include-url' ).prop( 'checked' ) ) {
+			jQuery( '#included-urls-container' ).show();
+		}
+
+		jQuery( '#include-url' ).change(
+			function() {
+				if ( this.checked ) {
+					jQuery( '#included-urls-container' ).fadeIn();
+				} else {
+					jQuery( '#included-urls-container' ).fadeOut();
+				}
+			}
+		);
+
+		if ( jQuery( '#exclude-url' ).prop( 'checked' ) ) {
 			jQuery( '#exlucded-urls-container' ).fadeIn();
-		} else {
-			jQuery( '#exlucded-urls-container' ).fadeOut();
 		}
-	});
-
-	if ( jQuery( '#include-url' ).prop( 'checked' ) ) {
-		jQuery( '#included-urls-container' ).show();
 	}
-
-	jQuery( '#include-url' ).change( function() {
-		if ( this.checked ) {
-			jQuery( '#included-urls-container' ).fadeIn();
-		} else {
-			jQuery( '#included-urls-container' ).fadeOut();
-		}
-	});
-
-	if ( jQuery( '#exclude-url' ).prop( 'checked' ) ) {
-		jQuery( '#exlucded-urls-container' ).fadeIn();
-	}
-});
+);
 
 function opentab( evt, tabName ) {
 	var i, tabcontent, tablinks;
@@ -71,5 +79,6 @@ function opentab( evt, tabName ) {
 	}
 
 	document.getElementById( tabName ).style.display = 'block';
+
 	evt.currentTarget.className += ' active';
 }
--- tawkto/assets/js/tawk.selection.js
+++ tawkto/assets/js/tawk.selection.js
@@ -1,27 +1,30 @@
 'use strict';
 
 var currentHost = window.location.protocol + '//' + window.location.host;
-var iframeUrl = tawkSelectionData.url.iframe + '&parentDomain=' + currentHost;
-var baseUrl = tawkSelectionData.url.base;
+var iframeUrl   = tawkSelectionData.url.iframe + '&parentDomain=' + currentHost;
+var baseUrl     = tawkSelectionData.url.base;
 
 jQuery( '#tawk-iframe' ).attr( 'src', iframeUrl );
 
-window.addEventListener( 'message', function( e ) {
-	if ( baseUrl === e.origin ) {
+window.addEventListener(
+	'message',
+	function( e ) {
+		if ( baseUrl === e.origin ) {
 
-		if ( 'setWidget' === e.data.action ) {
-			setWidget( e );
-		}
+			if ( 'setWidget' === e.data.action ) {
+				setWidget( e );
+			}
 
-		if ( 'removeWidget' === e.data.action ) {
-			removeWidget( e );
-		}
+			if ( 'removeWidget' === e.data.action ) {
+				removeWidget( e );
+			}
 
-		if ( 'reloadHeight' === e.data.action ) {
-			reloadIframeHeight( e.data.height );
+			if ( 'reloadHeight' === e.data.action ) {
+				reloadIframeHeight( e.data.height );
+			}
 		}
 	}
-});
+);
 
 function setWidget( e ) {
 	var data = {
@@ -30,22 +33,24 @@ function setWidget( e ) {
 		nonce: tawkSelectionData.nonce.setWidget
 	};
 
-	jQuery.ajax({
-		type: 'POST',
-		url: ajaxurl + '?action=tawkto_setwidget',
-		contentType: 'application/json',
-		dataType: 'json',
-		data: JSON.stringify( data ),
-		success: function( r ) {
-			if ( ! r.success ) {
-				return e.source.postMessage({ action: 'setFail' }, baseUrl );
+	jQuery.ajax(
+		{
+			type: 'POST',
+			url: ajaxurl + '?action=tawkto_setwidget',
+			contentType: 'application/json',
+			dataType: 'json',
+			data: JSON.stringify( data ),
+			success: function( r ) {
+				if ( ! r.success ) {
+					return e.source.postMessage({ action: 'setFail' }, baseUrl );
+				}
+				e.source.postMessage({ action: 'setDone' }, baseUrl );
+			},
+			error: function() {
+				e.source.postMessage({ action: 'setFail' }, baseUrl );
 			}
-			e.source.postMessage({ action: 'setDone' }, baseUrl );
-		},
-		error: function() {
-			e.source.postMessage({ action: 'setFail' }, baseUrl );
 		}
-	});
+	);
 }
 
 function removeWidget( e ) {
@@ -53,22 +58,24 @@ function removeWidget( e ) {
 		nonce: tawkSelectionData.nonce.removeWidget
 	};
 
-	jQuery.ajax({
-		type: 'POST',
-		url: ajaxurl + '?action=tawkto_removewidget',
-		contentType: 'application/json',
-		dataType: 'json',
-		data: JSON.stringify( data ),
-		success: function( r ) {
-			if ( ! r.success ) {
-				return e.source.postMessage({ action: 'removeFail' }, baseUrl );
+	jQuery.ajax(
+		{
+			type: 'POST',
+			url: ajaxurl + '?action=tawkto_removewidget',
+			contentType: 'application/json',
+			dataType: 'json',
+			data: JSON.stringify( data ),
+			success: function( r ) {
+				if ( ! r.success ) {
+					return e.source.postMessage({ action: 'removeFail' }, baseUrl );
+				}
+				e.source.postMessage({ action: 'removeDone' }, baseUrl );
+			},
+			error: function() {
+				e.source.postMessage({ action: 'removeFail' }, baseUrl );
 			}
-			e.source.postMessage({ action: 'removeDone' }, baseUrl );
-		},
-		error: function() {
-			e.source.postMessage({ action: 'removeFail' }, baseUrl );
 		}
-	});
+	);
 }
 
 function reloadIframeHeight( height ) {
