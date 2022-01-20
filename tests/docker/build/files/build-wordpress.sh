168a8d78eb5ff1d876e6b2e73a86aa4ac15fffc5
Alvin Johnson So <so.alvinjohnson@gmail.com>
2022-01-20 18:36:22 +0800

    moved docker folder to tests; applied a custom 'wait_for' command to wait for other containers to be ready;

--- tests/docker/build/files/build-wordpress.sh
+++ tests/docker/build/files/build-wordpress.sh
@@ -1,7 +1,40 @@
 #! /bin/sh
 
-# Wait for wordpress and db to fully load
-sleep 10;
+TIMEOUT=30;
+
+wait_for() {
+  for i in `seq $TIMEOUT` ; do
+    HOST=$(printf "%s\n" "$1"| cut -d : -f 1);
+    PORT=$(printf "%s\n" "$1"| cut -d : -f 2);
+
+    if [ -z $PORT ] || [ "$PORT" == "$HOST" ]; then
+      PORT="80";
+    fi;
+
+    nc -z "$HOST" "$PORT" > /dev/null 2>&1;
+
+    result=$?;
+    if [ $result -eq 0 ] ; then
+		echo "$1 is ready."
+      exit 0;
+    fi
+	echo "$1 is not yet ready."
+    sleep 1;
+  done
+  echo "Operation timed out" >&2;
+  exit 1;
+}
+
+FAILED=0;
+
+wait_for ${WEB_HOST} & WEB_PID=$!;
+wait_for ${WORDPRESS_DB_HOST} & DB_PID=$!;
+wait $WEB_PID || FAILED=$((FAILED+1));
+wait $DB_PID || FAILED=$((FAILED+1));
+
+if [ $FAILED -gt 0 ]; then
+  exit 1;
+fi
 
 # Run wp cli setup commands
 wp core install --path="/var/www/html" --url=http://${WEB_HOST} --title="Local Wordpress By Docker" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL};
